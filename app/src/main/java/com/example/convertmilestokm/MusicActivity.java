f1b9312611b24b815d9d6ad1fdc4e92af686ced4
flori <qwertz2002fr@yahoo.fr>
2022-03-18 15:28:18 +0100

    sound (1)

---
+++ app/src/main/java/com/example/convertmilestokm/MusicActivity.java
@@ -0,0 +1,22 @@
+package com.example.convertmilestokm;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+
+    public void playMusic(View v) {
+    }
+
+    public void pauseMusic(View v) {
+    }
+
+}
