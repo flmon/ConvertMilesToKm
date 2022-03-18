fa199539478adf5869fa05b97ecef62d8f1d58a6
flori <qwertz2002fr@yahoo.fr>
2022-03-18 17:31:32 +0100

    radiobutton / background

--- app/src/main/java/com/example/convertmilestokm/BackgroundActivity.java
+++ app/src/main/java/com/example/convertmilestokm/BackgroundActivity.java
@@ -1,43 +1,47 @@
 package com.example.convertmilestokm;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class BackgroundActivity extends AppCompatActivity implements View.OnClickListener {
-    RadioButton btnBlue, btnMagenta, btnYellow;
+public class BackgroundActivity extends AppCompatActivity {
+    static String COLOR_PREF = "COLOR_PREF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_background);
 
-        btnBlue = findViewById(R.id.radioButtonBlue);
-        btnBlue.setOnClickListener(this);
-        btnMagenta = findViewById(R.id.radioButtonMagenta);
-        btnMagenta.setOnClickListener(this);
-        btnYellow = findViewById(R.id.radioButtonYellow);
-        btnYellow.setOnClickListener(this);
+        SharedPreferences pref = getSharedPreferences(COLOR_PREF, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        RadioGroup rg = findViewById(R.id.rbgroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                ConstraintLayout layout = findViewById(R.id.layout);
+                int colorCode = 0;
+                switch (i) {
+                    case R.id.radioButtonBlue:
+                        colorCode = Color.BLUE;
+                        break;
+                    case R.id.radioButtonMagenta:
+                        colorCode = Color.MAGENTA;
+                        break;
+                    case R.id.radioButtonYellow:
+                        colorCode = Color.YELLOW;
+                        break;
+                }
+                layout.setBackgroundColor(colorCode);
+                editor.putInt(COLOR_PREF, colorCode);
+                editor.commit();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        ConstraintLayout layout = findViewById(R.id.layout);
-        switch (view.getId()) {
-            case R.id.radioButtonBlue:
-                layout.setBackgroundColor(Color.BLUE);
-                break;
-            case R.id.radioButtonMagenta:
-                layout.setBackgroundColor(Color.MAGENTA);
-                break;
-            case R.id.radioButtonYellow:
-                layout.setBackgroundColor(Color.YELLOW);
-                break;
-        }
-
-    }
 }
